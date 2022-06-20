4b2bad28d998d9a75a35965f3ba3ecc601215d7a
Chika Fujinyawa <107160298+fujianyawa@users.noreply.github.com>
2022-06-20 14:35:32 +0800

    refactor playerRound function

--- index.js
+++ index.js
@@ -3,35 +3,44 @@ const computerSelection = computerPlay();
 const playerSelection = playerPlay();
 
 function computerPlay() {
-    const randomPick = Math.floor(Math.random() * choices.length);
-    return (randomPick, choices[randomPick]);
+  const randomPick = Math.floor(Math.random() * choices.length);
+  return (randomPick, choices[randomPick]);
 }
 
 function playerPlay() {
-    const playerPick = prompt("Choose one: rock, paper, or scissors? ");
-    return playerPick;
+  const playerPick = prompt("Choose one: rock, paper, or scissors? ");
+  return playerPick;
 }
 
 function playRound(playerSelection, computerSelection) {
-     playerSelection,
-     computerSelection = choices;
-
-    if ((playerSelection == "rock" && computerSelection == "paper") || 
-    (playerSelection == "paper" && computerSelection == "rock")) {
-        return ("You lose, Paper beats Rock") || ("You win, Rock beats Paper")
-    } 
-    else if ((playerSelection == "scissor" && computerSelection == "rock" ) || 
-    (playerSelection == "rock" && computerSelection == "scissor")) {
-        return ("You lose, Rock beats Scissor") || ("You win, Rocks beats Scissor")
-    }
-    else if ((playerSelection == "paper" && computerSelection == "scissor" ) || 
-    (playerSelection == "scissor" && computerSelection == "paper")) {
-        return ("You lose, Scissor beats Paper") || ("You win, Scissor beats Paper")
-    } else {
-        (playerSelection == computerSelection)
-        return ("It's a tie")
-    }
+  if (playerSelection === computerSelection) {
+      return "It's a tie"
+  } else if ((playerSelection == "rock" && computerSelection == "scissor") || 
+             (playerSelection == "paper" && computerSelection == "rock")   || 
+             (playerSelection == "scissor" && computerSelection == "paper")) {
+      return "You win! ${playerSelection} beats the ${computerSelection}"
+  } else {
+      return "You lost! ${playerSelection} beats the ${computerSelection}"
+  }
 }
+
+    // refactor
+    // if ((playerSelection == "rock" && computerSelection == "paper") || 
+    // (playerSelection == "paper" && computerSelection == "rock")) {
+    //     return ("You lose, Paper beats Rock") || ("You win, Rock beats Paper")
+    // } 
+    // else if ((playerSelection == "scissor" && computerSelection == "rock" ) || 
+    // (playerSelection == "rock" && computerSelection == "scissor")) {
+    //     return ("You lose, Rock beats Scissor") || ("You win, Rocks beats Scissor")
+    // }
+    // else if ((playerSelection == "paper" && computerSelection == "scissor" ) || 
+    // (playerSelection == "scissor" && computerSelection == "paper")) {
+    //     return ("You lose, Scissor beats Paper") || ("You win, Scissor beats Paper")
+    // } else {
+    //     (playerSelection == computerSelection)
+    //     return ("It's a tie")
+    // }
+
 
 // const playerSelection = "rock";
 // console.log(playRound(playerSelection, computerSelection));
